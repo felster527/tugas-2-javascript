dfb833b094bd0882911ce5bbe7d58940b41581ee
felster527 <91239878+felster527@users.noreply.github.com>
2023-08-21 16:11:09 +0700

    Add files via upload

---
+++ Tugas2.js
@@ -0,0 +1,16 @@
+
+var angka = 7
+
+var huruf = "felix"
+
+
+var benar = true
+
+
+var isi = null
+var isi2 = undefined
+
+
+var data = [1,2,3,4,5,6,7]
+
+console.log("angka : "+angka+"\nString : "+huruf+"\nBoolean : "+benar+"\nNull dan Undefined : "+isi," ",isi2+"\nArray : "+data)
